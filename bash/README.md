0e25ed1c6b159deda709450c1176e26a6aa2bfef
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-05-22 17:43:28 -0500

    feat(bash): resetNet, color updates and more

---
+++ bash/README.md
@@ -0,0 +1,7 @@
+## ./checkIP.sh
+
+This tool check an IP for open ports.
+
+## ./resetNet.sh
+
+This tool reset network settings.
--- bash/resetNet.sh
+++ bash/resetNet.sh
@@ -1,49 +1,75 @@
+#!/bin/bash
 #
 # Author: Meyer Pidiache <github.com/meyer-pidiache>
 #
 
 set -euo pipefail
 
-greenColour="\e[0;32m\033[1m"
-redColour="\e[0;31m\033[1m"
-blueColour="\e[0;34m\033[1m"
-yellowColour="\e[0;33m\033[1m"
-endColour="\033[0m\e[0m"
+GREEN="\e[0;32m\033[1m"
+RED="\e[0;31m\033[1m"
+BLUE="\e[0;34m\033[1m"
+YELLOW="\e[0;33m\033[1m"
+ENDCOLOR="\033[0m\e[0m"
 
-function setManaged ()
-{
-  echo -e "[${greenColour}*${endColour}] Restarting network..."
+setManaged() {
+  echo -e "[${GREEN}*${ENDCOLOR}] Restarting network..."
+  ip link set $interface down
   # Deactivate monitor mode
-  iw dev | grep "monitor" >/dev/null
-  if [[ $(echo $?) -ne 1 ]]; then
-    ip link set wlan0 down
-    iw wlan0 set type managed
-    ip link set wlan0 up
-  fi
+  # iw dev | grep "monitor" >/dev/null
+  # if [[ $(echo $?) -ne 1 ]]; then
+  #   iw $interface set type managed
+  #   if [[ "$?" -ne 0 ]]; then
+  #     echo -e "[${RED}*${ENDCOLOR}] Failed to set managed interface"
+  #   else
+  #     echo -e "[${GREEN}*${ENDCOLOR}] $interface is now managed"
+  #   fi
+  # fi
+
   # Restarting network services
-  systemctl restart wpa_supplicant.service NetworkManager.service dhcpcd.service
-  iw dev
+  network_services=(wpa_supplicant.service NetworkManager.service dhcpcd.service)
+  for service in "${network_services[@]}"; do
+    systemctl restart $service 2>/dev/null
+    if [[ "$?" -ne 0 ]]; then
+      echo -e "[${RED}*${ENDCOLOR}]Failed to restart $service\n"
+    else
+      echo -e "[${GREEN}*${ENDCOLOR}] $service up"
+    fi
+  done
+
+  # Set default MAC
+  macchanger -p $interface
+  if [[ "$?" -ne 0 ]]; then
+    echo -e "[${RED}*${ENDCOLOR}] Failed to set default MAC"
+  fi
+
+  ip link set $interface up
+  echo -e "[${BLUE}*${ENDCOLOR}] Done!"
 }
 
 if [ "$(id -u)" == "0" ]; then
   # Getting arguments
-  declare -i parameter_counter=0;
+  declare -i parameter_counter=0
   while getopts ":i:" arg; do
     case $arg in
-      i) interface=$OPTARG; let parameter_counter+=1;;
+    i) interface=$OPTARG; let parameter_counter+=1;;
     esac
   done
 
   if [ $parameter_counter -ne 1 ]; then
-    setManaged
-    echo -e "\n[${redColour}*${endColour}] Use: resetNet -i [Interface Name]"
+    echo -e "\n[${RED}*${ENDCOLOR}] Use: resetNet -i [Default Interface]"
   else
-    # Process
-    echo -e "[${greenColour}*${endColour}] Deleting interface..."
-    ifconfig $interface down && iw dev $interface del
+    interfaces=($(iw dev | awk '$1=="Interface"{print $2}'))
 
-    setManaged
+    for i in "${interfaces[@]}"; do
+      if [ "$i" != "$interface" ]; then
+        # Process
+        echo -e "[${GREEN}*${ENDCOLOR}] Deleting $i interface"
+        ifconfig $i down && iw dev $i del
+      fi
+    done
+
+    setManaged $interface
   fi
 else
-  echo -e "\n[${yellowColour}*${endColour}] Execute as root\n"
+  echo -e "\n[${YELLOW}*${ENDCOLOR}] Execute as root\n"
 fi
