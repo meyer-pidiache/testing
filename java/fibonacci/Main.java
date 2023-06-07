c2baa0f6b0a0a1e292b0022056b3136e5348c6c4
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-06-07 16:09:33 -0500

    feat(java): fibonacci v0.2

--- java/fibonacci/Main.java
+++ java/fibonacci/Main.java
@@ -1,13 +1,41 @@
 import java.util.Scanner; 
+import java.util.Arrays;
 
 public class Main {
-  public static void main(String[] args) {
-    Scanner obj = new Scanner(System.in);
-    System.out.print("Type Fibonacci Length: ");
-    String position = obj.nextLine();
-
-    System.out.println(position);
-    
+  public static int getFibonacci(int number) {
+    if (number == 0) {
+      return 0;
+    } else if (number == 1) {
+      return 1;
+    } else {
+      int a = getFibonacci(number - 2);
+      int b = getFibonacci(number - 1);
+      return a + b;
+    }
   }
-  
-};
+
+  public static int[] getByRecursion(int position) {
+    int[] sequence;
+    sequence = new int[position];
+
+    for (int i = 0; i < position; i++) {
+      sequence[i] = getFibonacci(i);
+    }
+
+    return sequence;
+  }
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Type Fibonacci Length: ");
+    int position = sc.nextInt();
+    
+    if (position > 15) {
+      System.out.println("\nLineal mode implement");
+    } else {
+      int[] list = getByRecursion(position);
+      System.out.println("\n " + Arrays.toString(list));
+      System.out.println("\nRecursion mode implement");
+    }
+  }
+}
