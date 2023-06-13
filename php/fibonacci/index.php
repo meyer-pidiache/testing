0fa673e07b6e3d8a6eb2b9932e3c195d4d477570
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-06-12 20:32:09 -0500

    feat(php): fibonacci v1.0

--- php/fibonacci/index.php
+++ php/fibonacci/index.php
@@ -22,15 +22,46 @@ function getByRecursion(int $position) {
   return $sequence;
 }
 
+function getByLineal(int $position) {
+  $sequence = array();
+
+  $less2 = 0;
+  $less1 = 1;
+
+  array_push($sequence, $less2);
+  array_push($sequence, $less1);
+
+  for ($i=2; $i < $position ; $i++) { 
+    $new_v = $less2 + $less1;
+    array_push($sequence, $new_v);
+
+    $less2 = $less1;
+    $less1 = $new_v;
+  }
+
+  return $sequence;  
+}
+
 if ($position > 15) {
-  echo 1;
+  echo "Type Fibonacci Lenght: " . $position . "<br><br>";
+
+  $numbers = getByLineal($position);
+  foreach ($numbers as $key => $value) {
+    $list = "$list $value, ";
+  }
+  $list = substr($list, 1, -2);
+  echo "&nbsp;&nbsp;[$list]";
+
+  echo "<br><br>Lineal mode implement";
 } else {
   echo "Type Fibonacci Lenght: " . $position . "<br><br>";
+
   $numbers = getByRecursion($position);
   foreach ($numbers as $key => $value) {
     $list = "$list $value, ";
   }
   $list = substr($list, 1, -2);
   echo "[$list]";
+
   echo "<br><br>Recursion mode implement";
 }
