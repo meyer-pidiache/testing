9f74975c435a3d8651b43f95eb0a3c81bab68942
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-06-16 22:43:17 -0500

    feat(javascript): input

--- javascript/fibonacci/index.js
+++ javascript/fibonacci/index.js
@@ -1,1 +1,10 @@
-console.log("Hi");
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+readline.question('Type Fibonacci Length: ', name => {
+  console.log(`Length ${name}`);
+  readline.close();
+});
+
