72c270d711cc016100006e776e77dca521260678
Meyer Pidiache <meyer.pidiache@gmail.com>
2023-06-18 20:52:57 -0500

    feat(laravel): tailwindcss

--- php/laravel/devstagram/resources/views/layouts/app.blade.php
+++ php/laravel/devstagram/resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>@yield('titulo')</title>
-
+        @vite('resources/css/app.css    ')
     </head>
     <body>
         <nav>
@@ -13,7 +13,7 @@
             <a href="/tienda">Tienda</a>
             <a href="/nosotros">Sobre Nosotros</a>
         </nav>
-        <h1>@yield('titulo')</h1>
+        <h1 class="text-4xl">@yield('titulo')</h1>
         <hr>
         <p>@yield('contenido')</p>
     </body>
