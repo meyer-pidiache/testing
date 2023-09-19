6df8b26eba682cfe99689becb85302907617edaf
Meyer Pidiache <meyer-pidiache@gmail.com>
2023-09-18 21:34:28 -0500

    feat(c#): windows forms

---
+++ c#/windowsForms/DataGridView/Clase 5.3/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿
+namespace Clase_5._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Codigo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NombreP = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Precio = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.labelCodigo = new System.Windows.Forms.Label();
+            this.labelNombre = new System.Windows.Forms.Label();
+            this.labelPrecio = new System.Windows.Forms.Label();
+            this.textBoxCodigo = new System.Windows.Forms.TextBox();
+            this.textBoxNombre = new System.Windows.Forms.TextBox();
+            this.textBoxPrecio = new System.Windows.Forms.TextBox();
+            this.labelInformacion = new System.Windows.Forms.Label();
+            this.buttonAdicionar = new System.Windows.Forms.Button();
+            this.buttonBorrar = new System.Windows.Forms.Button();
+            this.buttonSumar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Codigo,
+            this.NombreP,
+            this.Precio});
+            this.dataGridView1.Location = new System.Drawing.Point(86, 80);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(344, 150);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Codigo
+            // 
+            this.Codigo.HeaderText = "Código Producto";
+            this.Codigo.Name = "Codigo";
+            // 
+            // NombreP
+            // 
+            this.NombreP.HeaderText = "Nombre Producto";
+            this.NombreP.Name = "NombreP";
+            // 
+            // Precio
+            // 
+            this.Precio.HeaderText = "Precio Producto";
+            this.Precio.Name = "Precio";
+            // 
+            // labelCodigo
+            // 
+            this.labelCodigo.AutoSize = true;
+            this.labelCodigo.Location = new System.Drawing.Point(522, 80);
+            this.labelCodigo.Name = "labelCodigo";
+            this.labelCodigo.Size = new System.Drawing.Size(46, 15);
+            this.labelCodigo.TabIndex = 1;
+            this.labelCodigo.Text = "Código";
+            // 
+            // labelNombre
+            // 
+            this.labelNombre.AutoSize = true;
+            this.labelNombre.Location = new System.Drawing.Point(522, 150);
+            this.labelNombre.Name = "labelNombre";
+            this.labelNombre.Size = new System.Drawing.Size(51, 15);
+            this.labelNombre.TabIndex = 2;
+            this.labelNombre.Text = "Nombre";
+            // 
+            // labelPrecio
+            // 
+            this.labelPrecio.AutoSize = true;
+            this.labelPrecio.Location = new System.Drawing.Point(522, 215);
+            this.labelPrecio.Name = "labelPrecio";
+            this.labelPrecio.Size = new System.Drawing.Size(40, 15);
+            this.labelPrecio.TabIndex = 3;
+            this.labelPrecio.Text = "Precio";
+            // 
+            // textBoxCodigo
+            // 
+            this.textBoxCodigo.Location = new System.Drawing.Point(588, 80);
+            this.textBoxCodigo.Name = "textBoxCodigo";
+            this.textBoxCodigo.Size = new System.Drawing.Size(100, 23);
+            this.textBoxCodigo.TabIndex = 4;
+            // 
+            // textBoxNombre
+            // 
+            this.textBoxNombre.Location = new System.Drawing.Point(604, 142);
+            this.textBoxNombre.Name = "textBoxNombre";
+            this.textBoxNombre.Size = new System.Drawing.Size(100, 23);
+            this.textBoxNombre.TabIndex = 5;
+            // 
+            // textBoxPrecio
+            // 
+            this.textBoxPrecio.Location = new System.Drawing.Point(604, 212);
+            this.textBoxPrecio.Name = "textBoxPrecio";
+            this.textBoxPrecio.Size = new System.Drawing.Size(100, 23);
+            this.textBoxPrecio.TabIndex = 6;
+            // 
+            // labelInformacion
+            // 
+            this.labelInformacion.AutoSize = true;
+            this.labelInformacion.Location = new System.Drawing.Point(86, 259);
+            this.labelInformacion.Name = "labelInformacion";
+            this.labelInformacion.Size = new System.Drawing.Size(38, 15);
+            this.labelInformacion.TabIndex = 7;
+            this.labelInformacion.Text = "label1";
+            // 
+            // buttonAdicionar
+            // 
+            this.buttonAdicionar.Location = new System.Drawing.Point(86, 297);
+            this.buttonAdicionar.Name = "buttonAdicionar";
+            this.buttonAdicionar.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdicionar.TabIndex = 8;
+            this.buttonAdicionar.Text = "Adicionar";
+            this.buttonAdicionar.UseVisualStyleBackColor = true;
+            this.buttonAdicionar.Click += new System.EventHandler(this.buttonAdicionar_Click);
+            // 
+            // buttonBorrar
+            // 
+            this.buttonBorrar.Location = new System.Drawing.Point(222, 297);
+            this.buttonBorrar.Name = "buttonBorrar";
+            this.buttonBorrar.Size = new System.Drawing.Size(75, 23);
+            this.buttonBorrar.TabIndex = 9;
+            this.buttonBorrar.Text = "Borrar";
+            this.buttonBorrar.UseVisualStyleBackColor = true;
+            this.buttonBorrar.Click += new System.EventHandler(this.buttonBorrar_Click);
+            // 
+            // buttonSumar
+            // 
+            this.buttonSumar.Location = new System.Drawing.Point(355, 297);
+            this.buttonSumar.Name = "buttonSumar";
+            this.buttonSumar.Size = new System.Drawing.Size(75, 23);
+            this.buttonSumar.TabIndex = 10;
+            this.buttonSumar.Text = "Sumar";
+            this.buttonSumar.UseVisualStyleBackColor = true;
+            this.buttonSumar.Click += new System.EventHandler(this.buttonSumar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonSumar);
+            this.Controls.Add(this.buttonBorrar);
+            this.Controls.Add(this.buttonAdicionar);
+            this.Controls.Add(this.labelInformacion);
+            this.Controls.Add(this.textBoxPrecio);
+            this.Controls.Add(this.textBoxNombre);
+            this.Controls.Add(this.textBoxCodigo);
+            this.Controls.Add(this.labelPrecio);
+            this.Controls.Add(this.labelNombre);
+            this.Controls.Add(this.labelCodigo);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Codigo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn NombreP;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Precio;
+        private System.Windows.Forms.Label labelCodigo;
+        private System.Windows.Forms.Label labelNombre;
+        private System.Windows.Forms.Label labelPrecio;
+        private System.Windows.Forms.TextBox textBoxCodigo;
+        private System.Windows.Forms.TextBox textBoxNombre;
+        private System.Windows.Forms.TextBox textBoxPrecio;
+        private System.Windows.Forms.Label labelInformacion;
+        private System.Windows.Forms.Button buttonAdicionar;
+        private System.Windows.Forms.Button buttonBorrar;
+        private System.Windows.Forms.Button buttonSumar;
+    }
+}
+
---
+++ c#/windowsForms/DataGridView/Clase 5.3/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase_5._3
+{
+    public partial class Form1 : Form
+    {
+        private int n = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonAdicionar_Click(object sender, EventArgs e)
+        {
+            int n = dataGridView1.Rows.Add();
+
+            dataGridView1.Rows[n].Cells[0].Value = textBoxCodigo.Text;
+            dataGridView1.Rows[n].Cells[1].Value = textBoxNombre.Text;
+            dataGridView1.Rows[n].Cells[2].Value = textBoxPrecio.Text;
+
+            textBoxCodigo.Text = "";
+            textBoxNombre.Text = "";
+            textBoxPrecio.Text = "";
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            n = e.RowIndex;
+
+            if (n != -1)
+            {
+                labelInformacion.Text = (string)dataGridView1.Rows[n].Cells[1].Value;
+            }
+        }
+
+        private void buttonBorrar_Click(object sender, EventArgs e)
+        {
+            if (n != -1)
+            {
+                dataGridView1.Rows.RemoveAt(n);
+            }
+        }
+
+        private void buttonSumar_Click(object sender, EventArgs e)
+        {
+            double resultado = 0;
+
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                resultado += Convert.ToDouble(row.Cells["Precio"].Value);
+            }
+
+            MessageBox.Show("La suma de los precios de los productos es $" + resultado);
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
