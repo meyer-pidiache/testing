ce5282d17eb7d0389292614c8178a1dd70502cc8
Meyer Pidiache <meyer-pidiache@gmail.com>
2023-09-19 08:22:31 -0500

    feat(c#): windows forms

---
+++ c#/windowsForms/Parcial2/Parcial2/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Parcial2
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labelNumero1 = new System.Windows.Forms.Label();
+            this.labelNumero2 = new System.Windows.Forms.Label();
+            this.labelResultadoSuma = new System.Windows.Forms.Label();
+            this.labelResultado = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(168, 84);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(357, 84);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(120, 23);
+            this.numericUpDown2.TabIndex = 1;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(310, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(21, 21);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "+";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(509, 86);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(21, 21);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "=";
+            // 
+            // labelNumero1
+            // 
+            this.labelNumero1.AutoSize = true;
+            this.labelNumero1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelNumero1.Location = new System.Drawing.Point(191, 126);
+            this.labelNumero1.Name = "labelNumero1";
+            this.labelNumero1.Size = new System.Drawing.Size(59, 21);
+            this.labelNumero1.TabIndex = 4;
+            this.labelNumero1.Text = "_______";
+            // 
+            // labelNumero2
+            // 
+            this.labelNumero2.AutoSize = true;
+            this.labelNumero2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelNumero2.Location = new System.Drawing.Point(382, 126);
+            this.labelNumero2.Name = "labelNumero2";
+            this.labelNumero2.Size = new System.Drawing.Size(59, 21);
+            this.labelNumero2.TabIndex = 5;
+            this.labelNumero2.Text = "_______";
+            // 
+            // labelResultadoSuma
+            // 
+            this.labelResultadoSuma.AutoSize = true;
+            this.labelResultadoSuma.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelResultadoSuma.Location = new System.Drawing.Point(573, 86);
+            this.labelResultadoSuma.Name = "labelResultadoSuma";
+            this.labelResultadoSuma.Size = new System.Drawing.Size(59, 21);
+            this.labelResultadoSuma.TabIndex = 6;
+            this.labelResultadoSuma.Text = "_______";
+            // 
+            // labelResultado
+            // 
+            this.labelResultado.AutoSize = true;
+            this.labelResultado.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.labelResultado.Location = new System.Drawing.Point(573, 126);
+            this.labelResultado.Name = "labelResultado";
+            this.labelResultado.Size = new System.Drawing.Size(59, 21);
+            this.labelResultado.TabIndex = 7;
+            this.labelResultado.Text = "_______";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 235);
+            this.Controls.Add(this.labelResultado);
+            this.Controls.Add(this.labelResultadoSuma);
+            this.Controls.Add(this.labelNumero2);
+            this.Controls.Add(this.labelNumero1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labelNumero1;
+        private System.Windows.Forms.Label labelNumero2;
+        private System.Windows.Forms.Label labelResultadoSuma;
+        private System.Windows.Forms.Label labelResultado;
+    }
+}
+
---
+++ c#/windowsForms/Parcial2/Parcial2/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Parcial2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private bool esPar(double numero)
+        {
+            return (numero % 2 == 0);
+        }
+
+        private void sumar()
+        {
+            double numero1 = Convert.ToDouble(numericUpDown1.Value);
+            double numero2 = Convert.ToDouble(numericUpDown2.Value);
+            double resultado = numero1 + numero2;
+
+            labelResultadoSuma.Text = Convert.ToString(resultado);
+
+            labelResultado.Text = esPar(resultado) ? "Par" : "Impar";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            labelNumero1.Text = esPar(Convert.ToDouble(numericUpDown1.Value)) ? "Par" : "Impar";
+            sumar();
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            labelNumero2.Text = esPar(Convert.ToDouble(numericUpDown2.Value)) ? "Par" : "Impar";
+            sumar();
+        }
+    }
+}
